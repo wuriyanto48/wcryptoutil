9e6f984e5902220b0a4e293614b189db9d7dcc99
Wuriyanto <wuriyanto48@yahoo.co.id>
2022-04-19 21:40:21 +0700

    more DRY

--- src/main/java/com/wuriyanto/wcryptoutil/WDigest.java
+++ src/main/java/com/wuriyanto/wcryptoutil/WDigest.java
@@ -10,24 +10,25 @@ public final class WDigest {
 
     public static String sha1(byte[] data) throws Exception {
         MessageDigest messageDigest = MessageDigest.getInstance(WAlgorithm.SHA_1_DIGEST);
-        messageDigest.update(data);
-        return WKeyUtil.hexEncode(messageDigest.digest());
+        return digest(messageDigest, data);
     }
 
     public static String sha256(byte[] data) throws Exception {
         MessageDigest messageDigest = MessageDigest.getInstance(WAlgorithm.SHA_256_DIGEST);
-        messageDigest.update(data);
-        return WKeyUtil.hexEncode(messageDigest.digest());
+        return digest(messageDigest, data);
     }
 
     public static String sha384(byte[] data) throws Exception {
         MessageDigest messageDigest = MessageDigest.getInstance(WAlgorithm.SHA_384_DIGEST);
-        messageDigest.update(data);
-        return WKeyUtil.hexEncode(messageDigest.digest());
+        return digest(messageDigest, data);
     }
 
     public static String sha512(byte[] data) throws Exception {
         MessageDigest messageDigest = MessageDigest.getInstance(WAlgorithm.SHA_512_DIGEST);
+        return digest(messageDigest, data);
+    }
+
+    private static String digest(MessageDigest messageDigest, byte[] data) {
         messageDigest.update(data);
         return WKeyUtil.hexEncode(messageDigest.digest());
     }
